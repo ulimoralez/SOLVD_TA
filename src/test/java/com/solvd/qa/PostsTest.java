5d9021ba25f42a103bc30a8af6e9c4b5151fb630
ulimoralez <43346918Uli>
2022-02-08 17:25:02 -0300

    Updated Test Runner

--- src/test/java/com/solvd/qa/PostsTest.java
+++ src/test/java/com/solvd/qa/PostsTest.java
@@ -4,6 +4,7 @@ import com.jayway.jsonpath.JsonPath;
 import com.qaprosoft.apitools.validation.JsonCompareKeywords;
 import com.qaprosoft.carina.core.foundation.AbstractTest;
 import com.solvd.qa.api.GetPostMethod;
+import com.solvd.qa.api.PatchPostMethod;
 import com.solvd.qa.api.PostPostMethod;
 import io.restassured.response.Response;
 import org.skyscreamer.jsonassert.JSONCompareMode;
@@ -12,19 +13,41 @@ import org.testng.annotations.Test;
 public class PostsTest extends AbstractTest{
 	
 	@Test
-	public void getPost(){
-		GetPostMethod getPostMethod = new GetPostMethod();
+	public void getPostWithId1Test(){
+		GetPostMethod getPostMethod = new GetPostMethod(1);
 		Response response = getPostMethod.callAPIExpectSuccess();
-		getPostMethod.validateResponse(JSONCompareMode.STRICT, JsonCompareKeywords.ARRAY_CONTAINS.getKey());
-		getPostMethod.validateResponseAgainstSchema( "api/user/get/rs.schema" );
+		getPostMethod.validateResponse();
+		getPostMethod.validateResponseAgainstSchema( "api/posts/get/rs.schema" );
 		
 		System.out.println("ID: "+JsonPath.read( response.asString(), "id"));
 	}
 	
 	@Test
-	public void postTest(){
+	public void postPostTest() {
 		PostPostMethod postPostMethod = new PostPostMethod();
 		Response response = postPostMethod.callAPIExpectSuccess();
-		postPostMethod.validateResponse(  );
+		postPostMethod.validateResponse();
+		postPostMethod.validateResponseAgainstSchema( "api/posts/get/rs.schema" );
 	}
+
+	@Test
+	public void postPatchTest(){
+		PostPostMethod postPostMethod = new PostPostMethod();
+		Response postResponse = postPostMethod.callAPIExpectSuccess();
+		postPostMethod.validateResponse();
+
+		Integer id = JsonPath.read(postResponse.asString(), "$.id");
+		String stringId = id.toString();
+		System.out.println("Id: "+stringId);
+
+		PatchPostMethod patchPostMethod = new PatchPostMethod(1);
+		Response response = patchPostMethod.callAPIExpectSuccess();
+		patchPostMethod.addProperty("id", id);
+		patchPostMethod.callAPIExpectSuccess();
+		patchPostMethod.validateResponse();
+		patchPostMethod.validateResponseAgainstSchema("api/posts/patch/patchSchema.json");
+
+	}
+
+
 }
