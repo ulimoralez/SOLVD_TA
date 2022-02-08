74ab4beab805d832f713b3d2163e5741d5706f3e
ulimoralez <43346918Uli>
2022-02-08 17:10:38 -0300

    Fixed GetPostMethod

--- src/main/java/com/solvd/qa/api/GetPostMethod.java
+++ src/main/java/com/solvd/qa/api/GetPostMethod.java
@@ -8,13 +8,14 @@ import com.qaprosoft.carina.core.foundation.api.http.HttpMethodType;
 import com.qaprosoft.carina.core.foundation.api.http.HttpResponseStatusType;
 import com.qaprosoft.carina.core.foundation.utils.R;
 
-@ResponseTemplatePath( path = "api/user/get/rs.json")
-@Endpoint(methodType = HttpMethodType.GET, url = "${post_api_url}/posts/1")
+@ResponseTemplatePath( path = "api/posts/get/rs.json")
+@Endpoint(methodType = HttpMethodType.GET, url = "${post_api_url}/posts/${id}")
 @SuccessfulHttpStatus( status = HttpResponseStatusType.OK_200)
 public class GetPostMethod extends AbstractApiMethodV2{
-	public GetPostMethod(){
+	public GetPostMethod(int id){
 		super();
 		replaceUrlPlaceholder( "post_api_url", R.CONFIG.get( "post_api_url" ) );
-		setProperties( "api/user/get/post.properties" );
+		replaceUrlPlaceholder("id", String.valueOf(1));
+		setProperties("api/posts/post.properties");
 	}
 }
