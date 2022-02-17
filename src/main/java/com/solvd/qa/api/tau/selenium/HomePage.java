ec85d08ce1deef2728efe5bbe2d8de528efa594d
ulimoralez <43346918Uli>
2022-02-17 16:12:33 -0300

    HomePage Class

---
+++ src/main/java/com/solvd/qa/api/tau/selenium/HomePage.java
@@ -0,0 +1,47 @@
+package com.solvd.qa.api.tau.selenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class HomePage {
+    private WebDriver driver;
+    private By formAuthenticationLink = By.linkText("Form Authentication");
+
+    public HomePage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public LoginPage clickFormAuthentication(){
+        clickLink("Form Authentication");
+        return new LoginPage(driver);
+    }
+
+    public DropdownPage clickDropdown(){
+        clickLink("Dropdown");
+        return new DropdownPage(driver);
+    }
+
+    public HoversPage clickHovers(){
+        clickLink("Hovers");
+        return new HoversPage(driver);
+    }
+
+    public KeyPressesPage clickKeyPresses(){
+        clickLink("Key Presses");
+        return new KeyPressesPage(driver);
+    }
+
+    public AlertsPage clickJavaScriptAlerts(){
+        clickLink("JavaScript Alerts");
+        return new AlertsPage(driver);
+    }
+
+    public FileUploadPage clickFileUpload(){
+        clickLink("File Upload");
+        return new FileUploadPage(driver);
+    }
+
+    private void clickLink(String linkText){
+        driver.findElement(By.linkText(linkText)).click();
+    }
+}
